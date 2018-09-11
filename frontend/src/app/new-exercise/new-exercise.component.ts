a25db194bb00d7d91e69503b9e51157c85708320
Victor Abeledo <victorabeledo@Victors-MacBook-Air.local>
2018-09-10 22:18:48 -0300

    add new exercise section

---
+++ frontend/src/app/new-exercise/new-exercise.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'new-exercise',
+  templateUrl: './new-exercise.component.html',
+  styleUrls: ['./new-exercise.component.scss']
+})
+export class NewExerciseComponent implements OnInit {
+
+  constructor(
+
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
