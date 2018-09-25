4b0c10415f7cdb2c4cb5fc125d01099891e7e0e0
Victor Abeledo <victorabeledo@Victors-MacBook-Air.local>
2018-09-24 21:33:23 -0300

    add new exercise component logic for word transformation

--- frontend/src/app/new-exercise/new-exercise.component.ts
+++ frontend/src/app/new-exercise/new-exercise.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import {CdkTextareaAutosize} from '@angular/cdk/text-field';
+import {take} from 'rxjs/operators';
 
 @Component({
   selector: 'new-exercise',
   templateUrl: './new-exercise.component.html',
   styleUrls: ['./new-exercise.component.scss']
 })
-export class NewExerciseComponent implements OnInit {
+export class NewExerciseComponent {
 
   constructor(
-
+    private ngZone: NgZone,
   ) { }
 
-  ngOnInit() {
-  }
+  @ViewChild('autosize') autosize: CdkTextareaAutosize;
 
+  triggerResize() {
+    // Wait for changes to be applied, then trigger textarea resize.
+    this.ngZone.onStable.pipe(take(1))
+        .subscribe(() => this.autosize.resizeToFitContent(true));
+  }
 }
